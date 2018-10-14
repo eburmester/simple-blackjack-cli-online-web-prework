b34ae353f69db2b8720a4e70e9096842bedbede7
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-14 22:32:21 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,7 +54,7 @@ end
 def runner
   welcome
   card_total = initial_round
-  loop do
+  hit? loop do
     if card_total < 21 
       card_total = hit?(card_total)
       display_card_total(card_total)
