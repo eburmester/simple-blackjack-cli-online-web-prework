c53e58f3043633b666fd84331204735e7324647e
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-14 22:00:21 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ end
 
 def hit?(card_total)
   prompt_user
-  ##get_user_input
+  #get_user_input
   user_input = get_user_input
   while user_input != "h" && user_input != "s"
     invalid_command
